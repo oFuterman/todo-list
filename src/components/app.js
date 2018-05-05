864070a68ccb2d87b4fe9b3e3b6ac442b58ad010
Omer <omerfu@gmail.com>
2018-05-04 17:41:35 -0700

    data to dom

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,13 @@ import React, { Component } from 'react';
 import List from './list';
 import AddItem from './add-items';
 import listData from '../helpers/list_data';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=poopfart';
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -14,38 +18,60 @@ class App extends Component {
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getListData();
     }
 
-    addItem(item){
-        this.setState({
-            listData: [item, ...this.state.listData]
-        });
+    async addItem(item) {
+        await axios.post(`${BASE_URL}/todos/${API_KEY}`,item);
+
+        this.getListData();
+        // this.setState({
+        //     listData: [item, ...this.state.listData]
+        // });
     }
 
-    deleteItem(index){
-        const listData = this.state.listData.slice();
+    async deleteItem(id) {
+        await axios.delete(`${BASE_URL}/todos/${id+API_KEY}`);
 
-        listData.splice(index,1);//this
+        this.getListData();
+        // const listData = this.state.listData.slice();
 
-        this.setState({
-            listData//is this
-        });
+        // listData.splice(index, 1);//this
+
+        // this.setState({
+        //     listData//is this
+        // });
     }
 
-    getListData(){
-        setTimeout(() => {
-            this.setState({ listData: listData });
-        }, 500);
+    async getListData() {
+
+        try {
+            const response = await axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+            this.setState({
+                listData: response.data.todos
+            });
+        }
+        catch(err){
+            console.log('ERROR:',err.message)
+        }
+
+        // axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp=>{
+        //     console.log('RESPONSE IS::::::::',resp.data.todos)
+
+        //     this.setState({
+        //         listData:resp.data.todos
+        //     })
+        // })
     }
 
-    render(){
+    render() {
         return (
             <div className="container">
                 <h1 className="center">To Do List</h1>
-                <AddItem add={this.addItem.bind(this)}/>
-                <List data={this.state.listData} delete={this.deleteItem.bind(this)}/>
+                <AddItem add={this.addItem.bind(this)} />
+                <List data={this.state.listData} delete={this.deleteItem.bind(this)} />
             </div>
         );
     }
